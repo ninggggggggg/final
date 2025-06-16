d6dacbabfbe88a2f2af3c542bb33debb59fc2c4a
Nguyen Phuong Linh <nplinh20112003@gmail.com>
2025-06-16 11:16:03 +0700

    testing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, session, render_template, send_from_directory, redirect, url_for
+from flask import Flask, request, jsonify, session, render_template, send_from_directory, redirect, url_for, render_template_string
 from ldap3 import Server, Connection, ALL, SUBTREE
 from werkzeug.utils import secure_filename
 import os
